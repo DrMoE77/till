980731fecc9ce94b01b4615084d4b94ff709ba1f
DrMoE77 <drmo.afifi@gmail.com>
2022-10-18 14:17:18 +1100

    upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/*function myFunction() {
+  var y = document.getElementById("onHundred").value;
+  var z = document.getElementById("fifty").value;
+  var x = y + z;
+  document.getElementById("add").innerHTML = x;
+}*/
+
+
+  
